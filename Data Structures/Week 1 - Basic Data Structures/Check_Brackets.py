1d34e8d7244e96cc464d5f66b55fe6de8309d0d6
Karan Waghela <65720485+karankrw@users.noreply.github.com>
2020-06-16 01:21:01 +0530

    Add files via upload

---
+++ Data Structures/Week 1 - Basic Data Structures/Check_Brackets.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 01:08:38 2020
+
+@author: karanwaghela
+"""
+
+import sys
+
+
+class Bracket:
+    
+    def __init__(self, bracket_type, position):
+        self.bracket_type = bracket_type
+        self.position = position
+        
+        
+    def match(self, char):
+        
+        if self.bracket_type == '{' and char == '}':
+            return True
+        if self.bracket_type == '[' and char == ']':
+            return True
+        if self.bracket_type == '(' and char == ')':
+            return True
+        return False
+    
+
+def checker(text):
+    stack = []
+    
+    for index, char in enumerate(text, start = 1):
+        if char in ("[", "(", "{"):
+            stack.append(Bracket(char, index))
+            
+        elif char in ("]", ")", "}"):
+            if not stack:
+                return index
+            
+            top = stack.pop()
+            if not top.match(char):
+                return index
+            
+    if stack:
+        top = stack.pop()
+        return top.position
+    
+    
+    return "Success"
+
+
+if __name__ == "__main__":
+    text = sys.stdin.read().strip("\n")
+    print(checker(text))
