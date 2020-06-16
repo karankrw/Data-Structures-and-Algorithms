07a8164f2a4a17214467197ff4a5bbd032a567ca
Karan Waghela <65720485+karankrw@users.noreply.github.com>
2020-06-17 01:02:01 +0530

    Add files via upload

---
+++ Data Structures/Week 2 - Priority Queues and Disjoint Sets/Build_Heap.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 01:37:52 2020
+
+@author: karanwaghela
+"""
+
+class HeapBuilder:
+    
+    def __init__(self):
+        self.swaps = []
+        self.array = []
+        
+        
+    @property
+    def size(self):
+        return len(self.array)
+    
+    
+    def read_data(self):
+        n = int(input())
+        self.array = [int(s) for s in input().split()]
+        assert n == self.size
+        
+        
+    def write_response(self):
+        print(len(self.swaps))
+        for swap in self.swaps:
+            print(swap[0], swap[1])
+            
+            
+    def l_child_index(self, index):
+        l_child_index = 2 * index + 1
+        if l_child_index >= self.size:
+            return -1
+        return l_child_index
+    
+    
+    def r_child_index(self, index):
+        r_child_index = 2 * index + 2
+        if r_child_index >= self.size:
+            return -1
+        return r_child_index
+    
+    
+    def sift_down(self, i):
+        min_index = i
+        l = self.l_child_index(i)
+        r = self.r_child_index(i)
+        
+        if l != -1 and self.array[l] < self.array[min_index]:
+            min_index = l
+        
+        if r != -1 and self.array[r] < self.array[min_index]:
+            min_index = r
+            
+        if i != min_index:
+            self.swaps.append((i, min_index))
+            self.array[i], self.array[min_index] = self.array[min_index], self.array[i]
+            self.sift_down(min_index)
+            
+            
+    def generate_swaps(self):
+        for i in range(self.size // 2, -1, -1):
+            self.sift_down(i)
+            
+    
+    def solve(self):
+        self.read_data()
+        self.generate_swaps()
+        self.write_response()
+        
+        
+
+if __name__ == '__main__':
+    heap = HeapBuilder()
+    heap.solve()
+    
+    
+    
+    
+    
+    
+    
+    
+        
+            
+    
+          
+        
