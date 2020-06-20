82cdc970d7071b49e01b56c9c0e10eca8c0a6880
Karan Waghela <65720485+karankrw@users.noreply.github.com>
2020-06-21 01:42:23 +0530

    Add files via upload

---
+++ Algorithms on Graphs/Week 1 - Decomposition 1/Reachability.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 22:08:34 2020
+
+@author: karanwaghela
+"""
+
+def reach(adj, x, y):
+    visited = [0] * len(adj)
+    return explore(adj, x, y, visited)
+
+
+def explore(adj, x, y, visited):
+    if x == y:
+        return 1
+    visited[x] = 1
+    for i in range(len(adj[x])):
+        if (not visited[adj[x][i]]):
+            if (explore(adj, adj[x][i], y, visited)):
+                return 1
+    return 0
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    adj = [[] for _ in range(n)]
+    for i in range(m):
+        a, b = map(int, input().split())
+        # adjacency list		
+        adj[a - 1].append(b - 1)
+        adj[b - 1].append(a - 1)
+    x, y = map(int, input().split())
+    print(reach(adj, x-1, y-1))
